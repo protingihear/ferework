f1174f9541464df4cbd551793f8743e41c4e8080
fharlup <fajarmufid01@gmail.com>
2025-03-04 10:13:12 +0700

    get comunity sama get post by id udah,yang belum create post,crete comunity,create reply,dan get reply

--- lib/services/comumnity_service.dart
+++ lib/services/comumnity_service.dart
@@ -3,14 +3,29 @@ import 'package:http/http.dart' as http;
 import '../models/community.dart';
 
 class ApiService {
+  static const String baseUrl = 'https://berework-production.up.railway.app/api';
+
+  // Ambil daftar komunitas
   static Future<List<Community>> fetchCommunities() async {
-    final response = await http.get(Uri.parse('http://localhost:5000/api/communities'));
+    final response = await http.get(Uri.parse('$baseUrl/communities'));
 
     if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+      List<dynamic> data = jsonDecode(response.body)['communities']; 
       return data.map((json) => Community.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load communities');
+    }
+  }
+
+  // Ambil daftar postingan berdasarkan ID komunitas
+  static Future<List<dynamic>> fetchCommunityPosts(int communityId) async {
+    final response = await http.get(Uri.parse('$baseUrl/communities/$communityId/posts'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body)['posts']; // Ambil array `posts`
+      return data;
+    } else {
+      throw Exception('Failed to load community posts');
     }
   }
 }
